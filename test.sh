5d53766951c53dadb7a79620140ade556e016dc5
Tomas Vidal <tomi_vidal_99@hotmail.com>
2022-08-05 19:10:06 -0300

    First commit

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# TODO:
+#   - make it only compile the gui code when it's necessary.
+
+# Executes the code for development.
+
+# Compiles the gui code to python.
+pyuic5 src/gui/app.ui > src/app/app_ui.py
+
+# Executes the app.
+python src/app/app.py
