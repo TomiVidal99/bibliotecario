3e8e9b2c7a12e8ac8293c033306edc7f3cff9b82
Tomas Vidal <tomi_vidal_99@hotmail.com>
2022-09-04 18:46:36 -0300

    Update: migrate all the custom scripts to Makefile

--- compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# TODO: all
-# Compiles the code for production.
--- editGui.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Script to execute the editor for creating the GUI
-designer-qt5 ./src/gui/app.ui & disown
